a81b52ca8d81083fd924730f9edad595435f051a
Vlad Cheshev <vlad.toloka37@yandex.ru>
2023-02-03 19:08:18 +0300

    feat: Сделать popup. 100%

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,20 @@ const EditButton = document.querySelector(".profile__button-edit");
 const popUp = document.querySelector(".popup");
 const closeButton = popUp.querySelector(".popup__btn-close");
 
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector("#name");
+const jobInput = document.querySelector("#description");
+
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+
+// Функция открытия, закрытия popUp.
+
 const handleEditButtonClick = () => {
   popUp.classList.add("popup__opened");
+
+  nameInput.value = profileName.textContent; // присваиваем инпуту name из профиля
+  jobInput.value = profileDescription.textContent; // присваиваем инпуту description из профиля
 };
 
 const handleCloseButtonClick = () => {
@@ -16,6 +28,19 @@ const handleOverlayClick = (event) => {
   }
 };
 
+// Обработчик отправки формы.
+
+const handleFormSubmit = (evt) => {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+
+  handleCloseButtonClick(); // Закрываем попап после отправки формы.
+};
+
+// Слушатетели
 EditButton.addEventListener("click", handleEditButtonClick);
 closeButton.addEventListener("click", handleCloseButtonClick);
 popUp.addEventListener("click", handleOverlayClick);
+formElement.addEventListener("submit", handleFormSubmit);
