9f7a490979d259f9e59cefbeb618f5766be77be7
Vlad Cheshev <vlad.toloka37@yandex.ru>
2023-02-04 23:50:58 +0300

    fix/Первая итерация.

--- scripts/index.js
+++ scripts/index.js
@@ -3,28 +3,28 @@ const popUp = document.querySelector(".popup");
 const closeButton = popUp.querySelector(".popup__btn-close");
 
 const formElement = document.querySelector(".popup__form");
-const nameInput = document.querySelector("#name");
-const jobInput = document.querySelector("#description");
+const nameInput = document.querySelector(".popup__input_type_name");
+const jobInput = document.querySelector(".popup__input_type_description");
 
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
 // Функция открытия, закрытия popUp.
 
-const handleEditButtonClick = () => {
-  popUp.classList.add("popup__opened");
+const openEditPopup = () => {
+  popUp.classList.add("popup_opened");
 
   nameInput.value = profileName.textContent; // присваиваем инпуту name из профиля
   jobInput.value = profileDescription.textContent; // присваиваем инпуту description из профиля
 };
 
 const handleCloseButtonClick = () => {
-  popUp.classList.remove("popup__opened");
+  popUp.classList.remove("popup_opened");
 };
 
 const handleOverlayClick = (event) => {
   if (event.target === event.currentTarget) {
-    popUp.classList.remove("popup__opened");
+    popUp.classList.remove("popup_opened");
   }
 };
 
@@ -40,7 +40,7 @@ const handleFormSubmit = (evt) => {
 };
 
 // Слушатетели
-EditButton.addEventListener("click", handleEditButtonClick);
+EditButton.addEventListener("click", openEditPopup);
 closeButton.addEventListener("click", handleCloseButtonClick);
 popUp.addEventListener("click", handleOverlayClick);
 formElement.addEventListener("submit", handleFormSubmit);
